aa0ec7bb855c0ddb5f0e4707fbb2ab3b6da38c99
Iago García <iagogarciaquintela@gmail.com>
2025-12-23 13:00:19 +0100

    Impostor para la cena

---
+++ Palabras.js
@@ -0,0 +1,6 @@
+// palabras.js
+const BIBLIOTECA_PALABRAS = [
+    "Pizza", "Messi", "Sushi", "Netflix", "Croquetas", 
+    "Pokémon", "TikTok", "Suegra", "Gimnasio", "Cerveza",
+    "Abuelo", "Reggaeton", "Minecraft", "WhatsApp", "Dormir"
+];
