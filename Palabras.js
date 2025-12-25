92ca881656770a565973a57b547aad41445a6b82
Iago García <iagogarciaquintela@gmail.com>
2025-12-25 19:57:17 +0100

    Más palabras

--- Palabras.js
+++ Palabras.js
@@ -1,7 +1,12 @@
-// palabras.js
 const BIBLIOTECA_PALABRAS = [
-    "Embajador", "carballo", "maceda", "vanitas", "gimnasio", "veganas",
-    "Lupe", "microrrelato", "Víctor vergas", "Minecraft", "interraíl",
-    "Vilagarcía", "pato", "carretera cortada", "chati", "palmeira",
-    "san juan", "chetao", "zona vieja", "USC", "san froilan", "Aldi", "kebab", "Jagger", "giratorio"
+    "Embajador", "Carballo", "Maceda", "Vanitas", "Gimnasio", "Veganas",
+    "Lupe", "Microrrelato", "Víctor vergas", "Minecraft", "Interraíl", "Vai rañar a pirola",
+    "Vilagarcía", "Pato", "Carretera cortada", "Sudaca", "Palmeira", "Latigazo",
+    "San Juan", "Chetao", "Zona vieja", "USC", "San froilán", "Aldi", "El cervecitas",
+    "Kebab", "Jagger", "Liñares", "Dacal", "Centro sociocultural", "Lobishome",
+    "Pistas", "Chaian", "Disco móvil chocolate", "Pablito", "2pa2", "Gaiás",
+    "Andrupi", "Chicochicle", "Chati", "Junito", "Praga", "China", "Gorda",
+    "Toromblito", "Capital", "Ruta", "Alopecia", "Juguete", "Chorizo", "Palique",
+    "Giratorio", "Dona Dana", "Casa abuela Xurxo", "Michigan", "Pádel", "Among Us",
+    "Vinicius"
 ];
